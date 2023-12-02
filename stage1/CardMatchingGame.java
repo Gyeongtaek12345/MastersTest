8ccb1bfdcb285d96971c9a1a7bc74f8bd1e54f2b
DESKTOP-GDDP7GQ\User <ogt124321@gmail.com>
2023-12-02 11:14:32 +0900

    카드들이 들어있는 cardDeck array를 만들고 카드들을 섞어주는 shuffle메서드를 만듬
    shuffle 메서드에서 collection에서의 shuffle을 사용하기위해 list로 변환후 순서를 섞어서 array로 반환해준다.

--- stage1/CardMatchingGame.java
+++ stage1/CardMatchingGame.java
@@ -1,7 +1,23 @@
 package stage1;
 
+import stage2.Application;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class CardMatchingGame {
+    public Integer[] cardDeck = {1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8};
     public static void main(String[] args) {
-        
+        CardMatchingGame game = new CardMatchingGame();         //card game에 대한 인스턴스
+        System.out.println(Arrays.toString(game.cardDeck));     //섞기전
+        game.shuffle();
+        System.out.println(Arrays.toString(game.cardDeck));     //섞은후
+    }
+    //Collection을 이용하여 cardDeck array를 list로 변환후에 shuffle을 사용해서 섞어준뒤 array로 변환해준다.
+    public void shuffle(){
+        List<Integer> deckList = Arrays.asList(cardDeck);
+        Collections.shuffle(deckList);
+        deckList.toArray(cardDeck);
     }
 }
