0b5013a3417f2e553a305aa9c77ea96f958cc894
DESKTOP-GDDP7GQ\User <ogt124321@gmail.com>
2023-12-02 11:35:13 +0900

    카드들을 콘솔에 6x3배열로 나열해서 보여주는 drawField 메서드 추가
    카드들의 값을 담을 2D array와 카드의 뒷면을 나타내는 x가들어간 2D array를 생성후  24장의 카드중 앞에서부터 18장의 카드를 루프문을 이용하여 array에 넣어준다.

--- stage1/CardMatchingGame.java
+++ stage1/CardMatchingGame.java
@@ -8,16 +8,55 @@ import java.util.List;
 
 public class CardMatchingGame {
     public Integer[] cardDeck = {1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8};
+    int row = 6;
+    int col = 3;
+    int[][] cardField = new int[col][row];
+    char[][] xField = new char[col][row];
+    char[][] temp = new char[col][row];
     public static void main(String[] args) {
         CardMatchingGame game = new CardMatchingGame();         //card game에 대한 인스턴스
-        System.out.println(Arrays.toString(game.cardDeck));     //섞기전
+//        System.out.println(Arrays.toString(game.cardDeck));     //섞기전
         game.shuffle();
-        System.out.println(Arrays.toString(game.cardDeck));     //섞은후
+//        System.out.println(Arrays.toString(game.cardDeck));     //섞은후
+        game.drawField();
+
     }
     //Collection을 이용하여 cardDeck array를 list로 변환후에 shuffle을 사용해서 섞어준뒤 array로 변환해준다.
     public void shuffle(){
         List<Integer> deckList = Arrays.asList(cardDeck);
         Collections.shuffle(deckList);
         deckList.toArray(cardDeck);
+    }
+    //cardDeck의 섞인 카드들을 cardField array에 넣어주고 뒤집혀있는 카드들인 xField array를 필드에 출력한다.
+    public void drawField(){
+        int count = 0;
+        for (int i=0; i<col; i++){
+            for (int j=0; j<row; j++){
+                cardField[i][j]=cardDeck[count];
+                count++;
+            }
+        }
+        for (int i=0; i<col; i++){
+            for (int j=0; j<row; j++){
+                xField[i][j]='x';
+            }
+        }
+        for (int i=0; i<col; i++){
+            for (int j=0; j<row; j++){
+                temp[i][j]='x';
+            }
+        }
+        for (int i=0; i<col; i++){                      //테스트 섞인 카드들을 출력
+            for (int j=0; j<row; j++){
+                System.out.print(cardField[i][j]+" ");
+            }
+            System.out.println();
+        }
+        for (int i=0; i<col; i++){
+            for (int j=0; j<row; j++){
+                System.out.print(xField[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
