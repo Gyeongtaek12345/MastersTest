955d0d877c5672ee1ea068af34ad9b2d21c7893c
DESKTOP-GDDP7GQ\User <ogt124321@gmail.com>
2023-12-03 11:56:53 +0900

    게임을 시작하기전에 설정을 관리해주는 setUp 메서드 추가 및 턴 시스템 구현
    setUp에서 시작전 2명의 플레이어의 이름을 받아오고 플레이터 1의 턴으로 초기화해준다.
    scoreCalcuate 메서드는 카드가 사라진때인 remove 메서드에서 불러오고 turnChanger 메서드는 reveal 메서드의 두 좌표값이 다를 경우 불려온다

--- stage2/Application.java
+++ stage2/Application.java
@@ -14,27 +14,24 @@ public class Application {
     char[][] temp = new char[col][row];
     int x,x2,y,y2,numTry,numTry2,gotScore,player1Score,player2Score;                    //플레이어가 선택한 좌표1,2  플레이어가 시도한횟수     //필드에 남은 카드의 수
     String player1Name, player2Name;
-    boolean turn,turnOver;
+    boolean turn;
     public static void main(String[] args) {
         Application game = new Application();          //card game에 대한 인스턴스
-
         System.out.println("!!!카드 맞추기 게임!!!");              //title
+        game.setUp();
         game.shuffle();
         game.drawField();
-        boolean eCheck = true;
         while(!game.winCondition()){
-            while(eCheck){
+            while(true){
                 try{
                     game.startGame();
                     System.out.println("________________________________________________________________________");
                     game.reveal();
-                    System.out.println("________________________________________________________________________");
-                    eCheck = false;
+                    break;
                 }catch (Exception e){
                     System.out.println("알맞은 좌표를 입력해주세요! (범위 0,0 ~ 2,5)");
                 }
             }
-            game.numTry++;                                       //시도 횟수 증가
             game.cardCount();
             System.out.println("________________________________________________________________________");
         }
@@ -45,17 +42,22 @@ public class Application {
 
     }
     public void setUp(){
-        System.out.println("플레이어1의 이름을 입력하세요: ");
+        System.out.print("플레이어1의 이름을 입력하세요: ");
         Scanner player1 = new Scanner(System.in);
         player1Name = player1.nextLine();
-        System.out.println("플레이어2의 이름을 입력하세요: ");
+        System.out.print("플레이어2의 이름을 입력하세요: ");
         Scanner player2 = new Scanner(System.in);
         player2Name = player2.nextLine();
         turn = true;                                            //플레이어1 턴 시작
     }
     //게임이 진행됨에있어 필요한 메세지의 출력과 게임에 사용될 2개의 좌표를 유저에게서 받아온다.
     public void startGame(){
-        System.out.println("시도 횟수: "+numTry+" 남은 카드 수: " + cardCount());
+        if (turn){
+            System.out.println("플레이어: " + player1Name + " 시도 횟수: "+numTry+" 남은 카드 수: " + cardCount() + " 점수: " + player1Score);
+        }else {
+            System.out.println("플레이어: " + player2Name + " 시도 횟수: "+numTry2+" 남은 카드 수: " + cardCount() + " 점수: " + player2Score);
+        }
+        //System.out.println("시도 횟수: "+numTry+" 남은 카드 수: " + cardCount());
         System.out.println("카드 위치에 해당하는 죄표를 2개 입력해주세요 eg.1,2");
         System.out.print("좌표1: ");
         Scanner coordinate = new Scanner(System.in);
@@ -95,12 +97,12 @@ public class Application {
                 temp[i][j]='x';
             }
         }
-//        for (int i=0; i<col; i++){                      //테스트 섞인 카드들을 출력
-//            for (int j=0; j<row; j++){
-//                System.out.print(cardField[i][j]+" ");
-//            }
-//            System.out.println();
-//        }
+        for (int i=0; i<col; i++){                      //테스트 섞인 카드들을 출력
+            for (int j=0; j<row; j++){
+                System.out.print(cardField[i][j]+" ");
+            }
+            System.out.println();
+        }
         makeField(xField);
     }
     //유저에게 받은 2개의 좌표의 값을 서로 비교하고 두값이 같을 경우 두카드를 제거하는 Remove()를 실행한다 그러나 두값이 다를경우 원래 상태로 돌아간다.
@@ -111,7 +113,7 @@ public class Application {
         temp[y2][x2] = Character.forDigit(pos2,10);
         makeField(temp);
         System.out.println("________________________________________________________________________");
-        if (pos1 == ' ' || pos2 == ' '){
+        if (pos1 == ' ' || pos2 == ' ' || x == x2 && y == y2){
             makeField(xField);
             temp[y][x] = 'x';
             temp[y2][x2] = 'x';
@@ -121,14 +123,26 @@ public class Application {
             if (pos2 == ' '){
                 temp[y2][x2] = ' ';
             }
+            System.out.println("올바른 2지점의 좌표를 입력해주세요! (카드가 없는 좌표와 좌표 중복 선택 불가)");
         }
         else if (pos1 == pos2){
+            if (turn){
+                numTry++;
+            }else{
+                numTry2++;
+            }
             remove();
         }
         else {
+            if (turn){
+                numTry++;
+            }else{
+                numTry2++;
+            }
             makeField(xField);
             temp[y][x] = 'x';
             temp[y2][x2] = 'x';
+            turnChanger();
         }
     }
     //좌표 값에 있는 array 값들을 제거하고 필드에 재출력한다.
@@ -140,6 +154,7 @@ public class Application {
         cardField[y][x] = ' ';
         cardField[y2][x2] = ' ';
         makeField(xField);
+        scoreCalculate();
     }
     //for 루프문을 이용하여 각각의 숫자가 array에 얼마나 있는지 체크한 후 모든 숙자가 2개 아래인 경우 조건문의 참값을 반환한다
     public boolean winCondition(){
@@ -177,27 +192,32 @@ public class Application {
         }
     }
     public void scoreCalculate(){
+        int score = 10;
         if (turn){
-            if (gotScore > 1){
-                for (int i=1;i<=gotScore;i++) {
-                    player1Score = player1Score*2;
+            if (gotScore >= 1){
+                for (int i = 0;i < gotScore;i++){
+                    score = score*2;
                 }
+                player1Score += score;
             }
             else {
                 player1Score += 10;
             }
-        }else{
-            if (gotScore > 1){
-                for (int i=1;i<=gotScore;i++) {
-                    player2Score = player2Score*2;
+        }else {
+            if (gotScore >= 1){
+                for (int i = 0;i < gotScore;i++){
+                    score = score*2;
                 }
+                player2Score += score;
             }
             else {
                 player2Score += 10;
             }
         }
+        gotScore++;
     }
     public void turnChanger(){
         turn = !turn;
+        gotScore = 0;
     }
 }
